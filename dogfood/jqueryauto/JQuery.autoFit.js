ddc016c67880b8920b9493644ee8116d8096a9d0
skyFi <fyl_2525@163.com>
2015-08-25 16:56:24 +0800

    Create JQuery.autoFit.js

---
+++ dogfood/jqueryauto/JQuery.autoFit.js
@@ -0,0 +1,21 @@
+;
+(function ($) {
+		$.fn.autoFit = function() {
+			var $this = $(this);
+			var oldWidth = parseInt($this.css('width'));
+			$this.keydown(function (event) {
+				var $this = $(this);
+				setTimeout(function() {
+					var val = $this.val().replace(/ /g, '&nbsp');
+					var fontSize = $this.css('font-size');
+					var fontFamily =  $this.css('font-family');
+					var padding = $this.outerWidth() - $this.width();
+					var contentWidth = $('<span id="autowidthforinputtext" style="font-size: ' + fontSize + '; padding: 0 ' + padding / 2 + 'px; font-family: ' + fontFamily + '; display: block; position: absolute; visibility: hidden;">' + val + '</span>').insertAfter($this).outerWidth();
+				    var newWidth = ((contentWidth + padding) > oldWidth) ? (contentWidth + padding) : oldWidth;
+					$('#autowidthforinputtext').remove();
+					$this.width(newWidth + 'px');
+				}, 0);
+			});
+			return $this;
+		};
+})(this['jQuery']);
