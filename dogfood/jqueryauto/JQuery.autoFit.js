cdc5eeea76eee4576d4b51ecaea0ff22b49b254e
skyFi <fyl_2525@163.com>
2015-08-26 14:45:23 +0800

    Update JQuery.autoFit.js

--- dogfood/jqueryauto/JQuery.autoFit.js
+++ dogfood/jqueryauto/JQuery.autoFit.js
@@ -11,7 +11,7 @@
 					var fontFamily =  $this.css('font-family');
 					var padding = $this.outerWidth() - $this.width();
 					var contentWidth = $('<span id="autowidthforinputtext" style="font-size: ' + fontSize + '; padding: 0 ' + padding / 2 + 'px; font-family: ' + fontFamily + '; display: block; position: absolute; visibility: hidden;">' + val + '</span>').insertAfter($this).outerWidth();
-				    var newWidth = ((contentWidth + padding) > oldWidth) ? (contentWidth + padding) : oldWidth;
+				    	var newWidth = ((contentWidth + padding) > oldWidth) ? (contentWidth + padding) : oldWidth;
 					$('#autowidthforinputtext').remove();
 					$this.width(newWidth + 'px');
 				}, 0);
